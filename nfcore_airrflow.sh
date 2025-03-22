d2a425aa0f9983286f571e18db254b30496d6a33
Dave Tang <davetingpongtang@gmail.com>
2025-03-22 22:33:09 +0900

    Run nfcore-airrflow

---
+++ nfcore_airrflow.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+export NXF_SINGULARITY_CACHEDIR=/data/nxf_singularity
+
+if [[ ! -e airr_rearrangement.tsv ]]; then
+   wget -q https://davetang.org/file/HumanB_Cell/outs/airr_rearrangement.tsv
+fi
+
+nextflow run /data/nf-core/airrflow/4_2_0/main.nf \
+   -profile singularity \
+   --input assembled_samplesheet.tsv \
+   --mode assembled \
+   --max_cpus 4 \
+   --outdir results
